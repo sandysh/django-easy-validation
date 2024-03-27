6b246f493c50c57451a851fca01b8c59ad5f74ef
Sandesh Satyal <sandesh@neodigitalnp.com>
2024-03-27 17:47:48 +0545

    Readme extension fixed and gitignore added to exclude build filess

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 # setup.py
 from setuptools import setup
 setup(
- name='easy_validation',
+ name='django_easy_validation',
  version='0.1',
  description='Easy Validation for Django based projects especially for Ajax based request.',
  url='https://github.com/sandysh/django-easy-validation',
  author='Sandesh Satyal',
  author_email='sandeshsatyal@gmail.com',
  license='MIT',
- packages=['easy_validation'],
- zip_safe=False
+ zip_safe=False,
 )
