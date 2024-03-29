6443629daf2d952812b3e9ffa83a2a55fcf9237c
Sandesh Satyal <sandesh@neodigitalnp.com>
2024-03-29 17:44:01 +0545

    readme updated validation method refined

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 setup(
  name='django-easy-validation',
- version='0.6',
+ version='0.8',
  description='Easy Validation for Django based projects especially for Ajax based request.',
  url='https://github.com/sandysh/django-easy-validation',
  author='Sandesh Satyal',
