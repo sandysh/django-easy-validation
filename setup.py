603f27b6f000a8528a272b8805d427eeb0f4be8b
Sandesh Satyal <sandesh@neodigitalnp.com>
2024-03-29 18:25:59 +0545

    project description added

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 # setup.py
 from setuptools import setup
+from os.path import join
+
+long_description = (
+    open('README.md').read())
+
 setup(
- name='django-easy-validation',
- version='0.8',
- description='Easy Validation for Django based projects especially for Ajax based request.',
- url='https://github.com/sandysh/django-easy-validation',
- author='Sandesh Satyal',
- author_email='sandeshsatyal@gmail.com',
- license='MIT',
- zip_safe=False,
+    name='django-easy-validation',
+    version='0.1',
+    description='Easy Validation for Django based projects especially for Ajax based request.',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    url='https://github.com/sandysh/django-easy-validation',
+    author='Sandesh Satyal',
+    author_email='sandeshsatyal@gmail.com',
+    license='MIT',
+    zip_safe=False,
+    keywords='django-easy-validation validation form-validation form ajax ajax-validation easy-validation',
 )
