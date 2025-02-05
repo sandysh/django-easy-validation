17090e32b6354df405f307bdb7401828628431ef
sandysh <sandeshsatyal@gmail.com>
2025-02-05 11:55:06 +0545

    version bumped

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-easy-validation',
-    version='0.0.1',
+    version='1.0.0',
     description='Easy AJAX validation for Django projects, inspired by Laravel approach.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
